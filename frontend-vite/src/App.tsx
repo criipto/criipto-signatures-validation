67c15138b6aee643d9b59242ff2845010d67d62b
nmoskaleva <natalia.moskaleva@criipto.com>
2023-09-10 13:54:17 +0200

    Add react-dropzone

--- frontend-vite/src/App.tsx
+++ frontend-vite/src/App.tsx
@@ -1,35 +1,58 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, {useCallback, useState} from 'react'
+import {useDropzone} from 'react-dropzone'
+import './App.css';
 
-function App() {
-  const [count, setCount] = useState(0)
+import type {Validation} from '../../library/src/index';
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+type ValidationResponse = {
+  [key: string]: Validation | {error: string}
 }
 
-export default App
+function App() {
+  const [files, setFiles] = useState<File[]>([]);
+  const onDrop = useCallback((acceptedFiles: File[]) => {
+    // Do something with the files
+    setFiles(acceptedFiles);
+  }, [])
+  const {getRootProps, getInputProps, isDragActive} = useDropzone({onDrop});
+
+  const handleUpload = async () => {
+    if (!files || !files.length) return;
+    const formData = new FormData();
+    for (const file of files) {
+      formData.append(file.name, file);
+    }
+
+    try {
+      const response = await fetch('/api/validate', {
+        method: 'POST',
+        body: formData,
+      });
+
+      if (response.ok) {
+        const results = await response.json() as ValidationResponse;
+        console.log(results);
+      } else {
+        console.error('HTTP error:', response.status);
+      }
+    } catch (error) {
+      console.error('Error uploading file:', error);
+    }
+  }
+
+  return (
+    <div className="App">
+      <div {...getRootProps()}>
+        <input {...getInputProps()} />
+        {
+          isDragActive ?
+            <p>Drop the files here ...</p> :
+            <p>Drag 'n' drop some files here, or click to select files</p>
+        }
+      </div>
+      <button onClick={handleUpload}>Upload</button>
+    </div>
+  );
+}
+
+export default App;
