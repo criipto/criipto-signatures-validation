a8111aecaab86fda16767351e23dfe63806b5df9
nmoskaleva <natalia.moskaleva@criipto.com>
2023-09-20 21:32:59 +0200

    Add font

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,11 +1,19 @@
 /** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
       backgroundColor: {
         'dark-purple': '#261d7b',
-        'light-purple': '#b9bcd7',
+        'light-purple': '#edeefb',
+        'bright-purple': '#5f4fec',
+        'light-orange': '#e3a48b',
       },
     },
   },
