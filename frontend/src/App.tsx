5d53d0a2d48f8bad04bebb05d8c8171e41e59b16
nmoskaleva <natalia.moskaleva@criipto.com>
2023-09-27 14:51:18 +0200

    Render file names in dropzone

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -13,6 +13,7 @@ type ValidationResponse = {
 function App() {
   const [files, setFiles] = useState<File[]>([]);
   const [results, setResults] = useState<ValidationResponse>();
+  const fileNames = files.map((file) => file.name);
 
   const onDrop = useCallback((acceptedFiles: File[]) => {
     setFiles(acceptedFiles);
@@ -49,7 +50,7 @@ function App() {
           <div className="px-4 py-5 sm:p-6">
             <h1 className="text-center px-2 py-1 rounded-md font-semibold text-4xl font-sans">Validate a Signature</h1>
             <p className="p-4 text-center">Here you can validate signed PDF files and view information included in the signatures.</p>
-            <DropzoneComponent onDrop={onDrop} handleUpload={handleUpload} />
+            <DropzoneComponent onDrop={onDrop} handleUpload={handleUpload} fileNames={fileNames} />
           </div>
         </div>
         {results && 'error' in results ? <p>Error validating file</p> : results && <ValidationResultsCard results={results} />}
--- frontend/src/Components/DropzoneComponent.tsx
+++ frontend/src/Components/DropzoneComponent.tsx
@@ -1,13 +1,45 @@
 import { useDropzone } from 'react-dropzone';
 
-export default function DropzoneComponent({ onDrop, handleUpload }: { onDrop: (acceptedFiles: File[]) => void; handleUpload: () => void }) {
+export default function DropzoneComponent({ onDrop, handleUpload, fileNames }: { onDrop: (acceptedFiles: File[]) => void; handleUpload: () => void; fileNames: string[] }) {
   const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
+
+  const style: React.CSSProperties = {
+    flex: 1,
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'start',
+    padding: '20px',
+    backgroundColor: '#fafafa',
+    color: 'gray',
+    minHeight: '200px',
+  };
+
+  function truncateFileName(fileName: string, maxLength: number) {
+    if (fileName.length <= maxLength) {
+      return fileName;
+    }
+    const start = fileName.slice(0, maxLength / 2);
+    const end = fileName.slice(-maxLength / 2);
+    return `${start}...${end}`;
+  }
 
   return (
     <div className="flex flex-col items-center justify-center rounded-md p-4">
-      <div {...getRootProps()} className="flex items-center justify-center h-36 md:w-6/12 border-2 border-dashed rounded-lg p-4 bg-gray-200">
+      <div {...getRootProps({ style })} className="flex items-center justify-center h-36 md:w-6/12 border-2 border-dashed rounded-lg p-4 bg-gray-200">
         <input {...getInputProps()} />
-        {isDragActive ? <p>Drop the files here ...</p> : <p>Drag 'n' drop a PDF file here, or click to select file</p>}
+        {fileNames.length > 0 && !isDragActive ? (
+          fileNames.map((fileName, index) => (
+            <ul>
+              <li key={index} className="max-w-xs">
+                {truncateFileName(fileName, 30)}
+              </li>
+            </ul>
+          ))
+        ) : isDragActive ? (
+          <p>Drop the files here ...</p>
+        ) : (
+          <p>Drag & drop PDF files here, or click to select files</p>
+        )}
       </div>
       <button onClick={handleUpload} className="mt-8 px-4 py-2 w-6/12 text-white font-semibold rounded-md bg-bright-purple hover:bg-dark-purple">
         Submit
