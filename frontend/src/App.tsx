7969642c074891863c7460ec31448a7213026b07
nmoskaleva <natalia.moskaleva@criipto.com>
2023-09-27 14:42:03 +0200

    Add footer and favicon

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import { useCallback, useState } from 'react';
 import DropzoneComponent from './Components/DropzoneComponent';
 import ValidationResultsCard from './Components/ValidationResultsCard';
+import Footer from './Components/Footer';
 import './App.css';
 
 import type { Validation } from '../../library/src/index';
@@ -42,15 +43,18 @@ function App() {
   };
 
   return (
-    <div className="lg:mx-24 lg:px-16">
-      <div className="overflow-hidden rounded-lg shadow mb-6 bg-light-purple">
-        <div className="px-4 py-5 sm:p-6">
-          <h1 className="text-center px-2 py-1 rounded-md font-semibold text-4xl font-sans">Validate a Signature</h1>
-          <p className="p-4 text-center">Here you can validate signed PDF files and view information included in the signatures.</p>
-          <DropzoneComponent onDrop={onDrop} handleUpload={handleUpload} />
+    <div className="flex flex-col h-[90vh] sm:h-[94vh] justify-between">
+      <div className="lg:mx-24 lg:px-16">
+        <div className="overflow-hidden rounded-lg shadow mb-6 bg-light-purple">
+          <div className="px-4 py-5 sm:p-6">
+            <h1 className="text-center px-2 py-1 rounded-md font-semibold text-4xl font-sans">Validate a Signature</h1>
+            <p className="p-4 text-center">Here you can validate signed PDF files and view information included in the signatures.</p>
+            <DropzoneComponent onDrop={onDrop} handleUpload={handleUpload} />
+          </div>
         </div>
+        {results && 'error' in results ? <p>Error validating file</p> : results && <ValidationResultsCard results={results} />}
       </div>
-      {results && 'error' in results ? <p>Error validating file</p> : results && <ValidationResultsCard results={results} />}
+      <Footer />
     </div>
   );
 }
---
+++ frontend/src/Components/Footer.tsx
@@ -0,0 +1,14 @@
+export default function Footer() {
+  return (
+    <footer aria-labelledby="footer">
+      <h2 id="footer-heading" className="sr-only">
+        Footer
+      </h2>
+      <div className="mx-auto max-w-7xl mb-8 sm:px-6 md:pt-12 lg:px-8">
+        <div className="flex justify-end lg:pb-2 mt-2">
+          <img src="/criipto_logo.svg" alt="Criipto logo" className="h-4 mt-2 sm:mt-8" />
+        </div>
+      </div>
+    </footer>
+  );
+}
