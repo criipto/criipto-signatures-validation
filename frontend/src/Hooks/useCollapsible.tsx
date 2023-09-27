93518a4668c582df73efa9b88a84ad00966d8bf0
nmoskaleva <natalia.moskaleva@criipto.com>
2023-09-27 10:15:52 +0200

    useMemo in useCollapsible hook

--- frontend/src/Hooks/useCollapsible.tsx
+++ frontend/src/Hooks/useCollapsible.tsx
@@ -1,18 +1,25 @@
-import { useState } from 'react';
+import { useState, useMemo, useCallback } from 'react';
 
 function useCollapsible(initialStates: boolean[]) {
   const [isOpenArray, setIsOpenArray] = useState(initialStates);
 
-  const toggleCollapse = (index: number) => {
-    const updatesIsOpenArray = [...isOpenArray];
-    updatesIsOpenArray[index] = !updatesIsOpenArray[index];
-    setIsOpenArray(updatesIsOpenArray);
-  };
+  const toggleCollapse = useCallback(
+    (index: number) => {
+      const updatesIsOpenArray = [...isOpenArray];
+      updatesIsOpenArray[index] = !updatesIsOpenArray[index];
+      setIsOpenArray(updatesIsOpenArray);
+    },
+    [isOpenArray]
+  );
 
-  return {
-    isOpenArray,
-    toggleCollapse,
-  };
+  const memoizedValue = useMemo(() => {
+    return {
+      isOpenArray,
+      toggleCollapse,
+    };
+  }, [isOpenArray, toggleCollapse]);
+
+  return memoizedValue;
 }
 
 export default useCollapsible;
