828eacfda537dd2d9a95599d901454e64ee7e394
Mick Hansen <maker@mhansen.io>
2023-09-29 11:37:08 +0200

    change text for unknown signatures

--- frontend/src/Components/UnknownSignature.tsx
+++ frontend/src/Components/UnknownSignature.tsx
@@ -8,7 +8,7 @@ export default function UnknownSignature(props: { signature: object; index: numb
   return (
     <div className="divide-y divide-gray-200 overflow-hidden rounded-lg bg-white shadow mx-8 my-8">
       <div className="px-4 py-5 text-white bg-red-400 text-5xl sm:px-6" onClick={() => toggleCollapse(index)}>
-        <h3 className="text-base font-semibold leading-7">Not a well-known signature</h3>
+        <h3 className="text-base font-semibold leading-7">Not a signature produced by Criipto</h3>
       </div>
       {isOpenArray[index] && <div className="px-4 py-5 sm:p-6 bg-gray-100">This signature type is unknown.</div>}
     </div>
